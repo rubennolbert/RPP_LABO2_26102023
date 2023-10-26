de8b8742a81d0f05552652512545e514ccb887ff
Rubén López <rubenlopez9891@gmail.com>
2023-10-26 18:22:48 -0300

    push de prueba

--- RPP_LABO2_26102023/Entidades/Base.cs
+++ RPP_LABO2_26102023/Entidades/Base.cs
@@ -1,6 +1,6 @@
 ﻿namespace Entidades
 {
-    public class Class1
+    public abstract class Base
     {
 
     }
---
+++ RPP_LABO2_26102023/Entidades/Hija2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public sealed class Hija2 : Base
+    {
+    }
+}
